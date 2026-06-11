9f46ea5aab8081df0913a7065f18df29201a5c46
mvbe14 <mvbeyey@gmail.com>
2026-06-11 19:22:11 +0300

    adding security options

--- auth-api/src/main/java/com/example/auth_api/service/JwtService.java
+++ auth-api/src/main/java/com/example/auth_api/service/JwtService.java
@@ -1,6 +1,7 @@
 package com.example.auth_api.service;
 
 import com.example.auth_api.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class JwtService {
@@ -30,5 +32,27 @@ public class JwtService {
                 .expiration(new Date(expiration))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public UUID extractUserId(String token) {
+        String userId = extractAllClaims(token).get("userId", String.class);
+        return UUID.fromString(userId);
+    }
+
+    public boolean isTokenValid(String token) {
+        Date expiration = extractAllClaims(token).getExpiration();
+        return expiration.after(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
     }
 }
